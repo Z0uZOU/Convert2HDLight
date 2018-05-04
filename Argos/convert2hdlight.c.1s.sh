03153da971b499b2df17db2a9e59ee3211fa13b0
Z0uZOU <30698692+Z0uZOU@users.noreply.github.com>
2018-05-04 16:09:55 +0200

    Update convert2hdlight.c.1s.sh

--- Argos/convert2hdlight.c.1s.sh
+++ Argos/convert2hdlight.c.1s.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="0.0.0.23"
+version="0.0.0.24"
 
 #### Nettoyage
 if [[ -f "~/convert2hdlight-update.sh" ]]; then
@@ -118,14 +118,10 @@ fi
 if [[ ! -f "$icons_cache/updater.png" ]] ; then curl -o "$icons_cache/updater.png" "https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/.cache-icons/updater.png" ; fi
 if [[ ! -f "$icons_cache/convert2hdlight-argos.png" ]] ; then curl -o "$icons_cache/convert2hdlight-argos.png" "https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/.cache-icons/hdlightencode-argos.png" ; fi
 if [[ ! -f "$icons_cache/convert2hdlight-bad-argos.png" ]] ; then curl -o "$icons_cache/convert2hdlight-bad-argos.png" "https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/.cache-icons/hdlightencode-bad-argos.png" ; fi
-if [[ ! -f "$icons_cache/movie_folder.png" ]] ; then curl -o "$icons_cache/movie_folder.png" "https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/.cache-icons/movie_folder.png" ; fi
-if [[ ! -f "$icons_cache/movie_file.png" ]] ; then curl -o "$icons_cache/movie_file.png" "https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/.cache-icons/movie_file.png" ; fi
 
 #### Mise en variable des icones
 CONVERT2HDLIGHT_ICON=$(curl -s "file://$icons_cache/convert2hdlight-argos.png" | base64 -w 0)
 CONVERT2HDLIGHT_BAD_ICON=$(curl -s "file://$icons_cache/convert2hdlight-bad-argos.png" | base64 -w 0)
-FOLDER_ICON=$(curl -s "file://$icons_cache/movie_folder.png" | base64 -w 0)
-FiLE_ICON=$(curl -s "file://$icons_cache/movie_file.png" | base64 -w 0)
 
 #### Fonction: dehumanize
 dehumanise() {
