c213236815f8a772da8458d770412e967ba97417
FRONTEAU Adrien <zouzou.is.reborn@hotmail.fr>
2020-11-12 14:09:16 +0100

    Fix chmod again

--- convert2hdlight.sh
+++ convert2hdlight.sh
@@ -14,7 +14,7 @@
 ## Installation bin: wget -q https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/convert2hdlight.sh -O convert2hdlight.sh && sed -i -e 's/\r//g' convert2hdlight.sh && shc -f convert2hdlight.sh -o convert2hdlight.bin && chmod +x convert2hdlight.bin && rm -f *.x.c && rm -f convert2hdlight.sh
 ## Installation sh: wget -q https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/convert2hdlight.sh -O convert2hdlight.sh && sed -i -e 's/\r//g' convert2hdlight.sh && chmod +x convert2hdlight.sh
 ## Micro-config
-version="Version: 0.0.1.87" #base du système de mise à jour
+version="Version: 0.0.1.88" #base du système de mise à jour
 description="Convertisseur en HDLight" #description pour le menu
 script_github="https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/convert2hdlight.sh" #emplacement du script original
 changelog_github="https://raw.githubusercontent.com/Z0uZOU/Convert2HDLight/master/Changelog/convert2hdlight" #emplacement du changelog de ce script
@@ -1315,7 +1315,9 @@ if [[ "$mes_medias" != "" ]] ; then
   done
   #### Désactivation du script argos
   if [[ -f "$chemin_argos/convert2hdlight.c.1s.sh" ]]; then
-    chmod -x "$chemin_argos/convert2hdlight.c.1s.sh"
+    if [[ -x "$chemin_argos/convert2hdlight.c.1s.sh" ]]; then
+      chmod -x "$chemin_argos/convert2hdlight.c.1s.sh"
+    fi
   fi
   
   #### Suppression des dossiers vides
